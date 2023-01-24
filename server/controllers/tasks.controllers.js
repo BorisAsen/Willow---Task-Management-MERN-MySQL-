8e831506deecbb431852a7cdea1c0201a02ad5bc
BorisAsen <borisyasen@gmail.com>
2023-01-24 18:37:37 -0300

    Manejo de errores de las funciones del CRUD (trycatch)

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -5,42 +5,60 @@ import { pool } from "../db.js"
 
 // Funcion para crear una tarea
 export const createTask = async (req, res) => {
-    //res.send('Creando tareas');
-    const {title, description} = req.body;
-    const [result] = await pool.query(
-        "INSERT INTO tasks(title, description) VALUES (?, ?)",
-        [title, description]
-    );
-    console.log(result);
-    res.json({
-        id: result.insertId,
-        title,
-        description,
-    }); 
+    try {
+        //res.send('Creando tareas');
+        const {title, description} = req.body;
+        const [result] = await pool.query(
+            "INSERT INTO tasks(title, description) VALUES (?, ?)",
+            [title, description]
+        );
+        console.log(result);
+        res.json({
+            id: result.insertId,
+            title,
+            description,
+        }); 
+    } catch (error) {
+        /* Devuelve un mensaje de error con estado http 500 que indica
+        que se acepto la solicitud pero un error impidio que se cumpliera */
+        return res.status(500).json({message: error.message}); 
+    }
+    
 }
 
 // Funcion para obtener todas las tareas de la db
 export const getTasks = async (req, res) => {
-    //res.send('Obteniendo tareas')
-    const [result] = await pool.query("SELECT * FROM tasks ORDER BY createAt ASC");
-    // Devuelve un arreglo con todas las tareas
-    console.log(result);
-    res.json(result);
+    try {
+        //res.send('Obteniendo tareas')
+        const [result] = await pool.query("SELECT * FROM tasks ORDER BY createAt ASC");
+        // Devuelve un arreglo con todas las tareas
+        console.log(result);
+        res.json(result); 
+    } catch (error) {
+        // Retornar el mensaje de error como respuesta
+        return res.status(500).json({message: error.message});
+    }
+    
 }
 
 // Funcion para obtener una tarea mediante su id
 export const getTask = async (req, res) => {
-    //res.send('Obteniendo una tarea')
-    const [result] = await pool.query("SELECT * FROM tasks WHERE id = ?", [req.params.id]);
-    // Devuelve un arreglo de una sola tarea que coincide con el ID solicitado
-    console.log(result);
-    // Controlar que se devuelva un resultado de error si no existe el id solicitado
-    if (result.length === 0) {
-        return res.status(404).json({message: "Task not found"});
-    };
-    // Como se esta buscando por id, el resultado sera siempre unico,
-    // Por lo que indico que el resultado sera el primer elemento del arreglo
-    res.json(result[0]);
+    try {
+        //res.send('Obteniendo una tarea')
+        const [result] = await pool.query("SELECT * FROM tasks WHERE id = ?", [req.params.id]);
+        // Devuelve un arreglo de una sola tarea que coincide con el ID solicitado
+        console.log(result);
+        // Controlar que se devuelva un resultado de error si no existe el id solicitado
+        if (result.length === 0) {
+            return res.status(404).json({message: "Task not found"});
+        };
+        // Como se esta buscando por id, el resultado sera siempre unico,
+        // Por lo que indico que el resultado sera el primer elemento del arreglo
+        res.json(result[0]);
+    } catch (error) {
+        // Retornar el mensaje de error como respuesta
+        return res.status(500).json({message: error.message});
+    }
 }
 
 // Funcion para eliminar una tarea mediante su id
@@ -60,14 +78,14 @@ export const deleteTask = async (req, res) => {
     return res.sendStatus(204);
 }
 
-// Funcion para actualizar una tarea mediante su id
+// Funcion para modificar una tarea mediante su id
 export const updateTask = async (req, res) => {
-    //res.send('Actualizando una tarea')
-    // Los datos de los campos a actualizar se obtienen del req.body
+    //res.send('Modificando una tarea')
+    // Los datos de los campos a modificar se obtienen del req.body
     const result = await pool.query("UPDATE tasks SET ? WHERE id = ?", [req.body, req.params.id]);
     /* 
        De la misma manera que al eliminar una tarea hay que controlar que exista,
-       lo mismo debe suceder al intentar actualizarla
+       lo mismo debe suceder al intentar modificarla
     */
     if (result.affectedRows === 0) {
         return res.status(404).json({message: "Task not found"});
