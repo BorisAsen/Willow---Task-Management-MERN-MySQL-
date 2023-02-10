5ae1f02838e116af9558ebf65ef0cf82f3b88220
BorisAsen <borisyasen@gmail.com>
2023-02-09 22:43:14 -0300

    Configurando lo necesario para el Build de la aplicacion. Test primer Build

--- server/index.js
+++ server/index.js
@@ -14,6 +14,11 @@ import taskRoutes from "./routes/tasks.routes.js";
 // Importar el modulo de cors
 import cors from "cors";
 
+// Importar las funciones necesarias para generar una constante, extraer la 
+// ruta absoluta de los archivos de la carpeta dist y luego distribuirlos
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
 // Indicar que app utilice el modulo cors para permitir peticiones desde el servidor de desarrollo del frontend
 app.use(cors());
 
@@ -30,6 +35,14 @@ app.use(indexRoutes);
 
 // Indicar que app haga uso de las rutas definidas en task.routes.js
 app.use(taskRoutes);
+
+// Extraer la ruta absoluta de los archivos en una constante
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
+
+// Distribuir los archivos estaticos de la carpeta dist
+app.use(express.static(join(__dirname, '../client/dist')));
+
 
 
 
